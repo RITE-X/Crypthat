a3400da938f32b46d7067ac6551c552d1bd4d13a
RITE-X <re.ritex@gmail.com>
2021-11-13 13:02:28 +0200

    The main methods of events and also the interface are written.

--- Crypthat/MainWindow.xaml.cs
+++ Crypthat/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Crypthat.Models;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Crypthat
 {
@@ -23,6 +13,57 @@ namespace Crypthat
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        BlockChain blocks;
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var selectedBlock = BlocksList.SelectedIndex;
+            BlockContent.Text = blocks[selectedBlock].Data;
+
+
+   
+        }
+
+        private void ChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedBlock = BlocksList.SelectedIndex;
+            if (selectedBlock is -1) { MessageBox.Show("Выберите блок!", "BitSY", MessageBoxButton.OK, MessageBoxImage.Warning); return; }
+            if (blocks is null) { MessageBox.Show("Добавьте хоть один блок!", "BitSY", MessageBoxButton.OK, MessageBoxImage.Warning); return; }
+
+            blocks.Insert(selectedBlock,BlockContent.Text);
+
+            BlocksData.Text = blocks.ToString();
+
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(BlockContent.Text)) { MessageBox.Show("Введите текст блока!", "BitSY", MessageBoxButton.OK, MessageBoxImage.Warning); return; }
+            if (blocks is null)
+            {
+                blocks = new BlockChain(BlockContent.Text);
+            }
+            else
+            {
+                blocks.Add(BlockContent.Text);
+            }
+            BlocksList.Items.Add(blocks[^1].Data);
+
+
+            BlockContent.Text = "";
+            BlocksData.Text = blocks.ToString();
+
+        }
+
+        private void BlockContent_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(e.Key==Key.Enter)
+            {
+                AddButton_Click(this, new RoutedEventArgs());
+            }
         }
     }
 }
