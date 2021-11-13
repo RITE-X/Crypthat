e77c6d78bf94e7b9aaa04b435d61e48f27d6cd37
RITE-X <re.ritex@gmail.com>
2021-11-13 13:00:17 +0200

    Block class described

---
+++ Crypthat/Models/Block.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Crypthat.Models
+{
+    internal class Block
+    {
+        internal byte[] Hash { get; private set; }
+
+        internal string Data { get; private set; }
+
+        internal Block(string data)
+        {
+            Data = data;
+            ComputeHash();
+        }
+
+        internal Block(byte[] hash, string data)
+        {
+            Data = data;
+            ReComputeHash(hash);
+        }
+
+        private void ComputeHash()
+        {
+            Hash = SHA256.Create().ComputeHash(Encoding.Latin1.GetBytes(Data));
+        }
+
+        public void ReComputeHash(byte[] _previousHash)
+        {
+            Hash = SHA256.Create().ComputeHash(_previousHash.Concat(Encoding.Latin1.GetBytes(Data)).ToArray());
+        }
+
+        public void ReComputeHash(byte[] _previousHash, string data)
+        {
+            Data = data;
+            Hash = SHA256.Create().ComputeHash(_previousHash.Concat(Encoding.Latin1.GetBytes(Data)).ToArray());
+        }
+
+        public override string ToString()
+        {
+            return $"{Data} -> {Convert.ToBase64String(Hash)}\n";
+        }
+    }
+}
+
